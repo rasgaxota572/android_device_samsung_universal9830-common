b8f0e90b49724a83572b7381b886fb481f84f361
rasgaxota572 <rasgaxota575@gmail.com>
2026-03-27 20:14:03 -0300

    Refactor BoardConfigCommon.mk with new configurations

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,22 +1,7 @@
-#
-# Copyright (C) 2023-2025 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 COMMON_PATH := device/samsung/universal9830-common
 
-# Architecture
+Architecture
+
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
@@ -28,28 +13,46 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a55
 
-# Bluetooth
+Bluetooth
+
 BOARD_HAVE_BLUETOOTH := true
 
-# Compatibility Matrix
+Compatibility Matrix
+
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
-# Dynamic Partitions
+Dynamic Partitions
+
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 9432989696
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext vendor product odm
 BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
 BOARD_SUPER_PARTITION_SIZE := 9437184000
 -include vendor/lineage/config/BoardConfigReservedSize.mk
 
-# DTB
+===============================
+
+🔥 KERNEL PREBUILT (SEM BUILD)
+
+===============================
+
+Kernel (Image separado)
+
+TARGET_PREBUILT_KERNEL := $(COMMON_PATH)/prebuilt/Image
+BOARD_KERNEL_IMAGE_NAME := Image
+
+DTB separado (OBRIGATÓRIO nesse caso)
+
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_DTB_CFG := $(COMMON_PATH)/configs/kernel/exynos990.cfg
+BOARD_PREBUILT_DTBIMAGE_DIR := $(COMMON_PATH)/prebuilt/dtb/
 
-# DTBO
-BOARD_KERNEL_SEPARATED_DTBO := true
-BOARD_DTBO_CFG := $(COMMON_PATH)/configs/kernel/$(TARGET_DEVICE).cfg
+DTBO
 
-# Filesystem
+BOARD_PREBUILT_DTBOIMAGE := $(COMMON_PATH)/prebuilt/dtbo.img
+
+===============================
+
+Filesystem
+
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := erofs
 BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
@@ -62,26 +65,30 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
-# Firmware
+Firmware
+
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
-# Framework Matrix
+Framework Matrix
+
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml
 
-# Graphics
+Graphics
+
 TARGET_USES_VULKAN := true
 
-# HIDL Manifest
+HIDL Manifest
+
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
 ifneq ($(TARGET_DEVICE),r8s)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/hubble/manifest.xml
 endif
 
-# Kernel
-BOARD_KERNEL_IMAGE_NAME := Image
+Boot config
+
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_MKBOOTIMG_ARGS := --base 0x10000000
@@ -91,19 +98,15 @@ BOARD_MKBOOTIMG_ARGS += --kernel_offset 0x00008000
 BOARD_MKBOOTIMG_ARGS += --pagesize 2048
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset 0x01000000
 BOARD_MKBOOTIMG_ARGS += --tags_offset 0x00000100
-#BOARD_KERNEL_CMDLINE := The bootloader ignores the cmdline from the boot.img
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_USE_LZ4 := true
-TARGET_KERNEL_CLANG_VERSION := r416183b
-TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/kernel/$(HOST_PREBUILT_TAG)/clang-$(TARGET_KERNEL_CLANG_VERSION)
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-gnu-
-TARGET_KERNEL_CONFIG := exynos9830_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/universal9830
 
-# Metadata
+Metadata
+
 BOARD_USES_METADATA_PARTITION := true
 
-# Partitions
+Partitions
+
 BOARD_BOOTIMAGE_PARTITION_SIZE := 61865984
 BOARD_CACHEIMAGE_PARTITION_SIZE := 629145600
 BOARD_DTBOIMG_PARTITION_SIZE := 8388608
@@ -111,44 +114,53 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 69009408
 BOARD_ROOT_EXTRA_FOLDERS := efs
 
-# Platform
+Platform
+
 BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := universal990
 TARGET_BOOTLOADER_BOARD_NAME := exynos990
 TARGET_SOC := exynos990
 
-# Properties
+Properties
+
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 
-# Recovery
+Recovery
+
 BOARD_HAS_DOWNLOAD_MODE := true
 BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_USES_FULL_RECOVERY_IMAGE := true
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/configs/init/etc/fstab.exynos990
 TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
 
-# Releasetools
+Releasetools
+
 TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
 
-# RIL
+RIL
+
 ENABLE_VENDOR_RIL_SERVICE := true
 
-# SECComp filters
+SECComp filters
+
 BOARD_SECCOMP_POLICY += $(COMMON_PATH)/seccomp
 
-# Security
+Security
+
 VENDOR_SECURITY_PATCH := 2025-07-01
 
-# SELinux
+SELinux
+
 include device/lineage/sepolicy/exynos/sepolicy.mk
 BOARD_SEPOLICY_TEE_FLAVOR := teegris
 include device/samsung_slsi/sepolicy/sepolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
-# Verified Boot
+AVB (se boot falhar, muda pra false)
+
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
@@ -157,8 +169,10 @@ BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
-# Call Samsung LSI board support package
+Samsung BSP
+
 include hardware/samsung_slsi-linaro/config/BoardConfig9830.mk
 
-# Call the proprietary setup
+Vendor
+
 include vendor/samsung/universal9830-common/BoardConfigVendor.mk
